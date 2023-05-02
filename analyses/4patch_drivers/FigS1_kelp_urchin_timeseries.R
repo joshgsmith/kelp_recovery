a528c223a46447a5bc6c623d3168cb02f0019e92
Joshua Smith <jossmith@MBA200226.mbayaq.org>
2023-05-02 08:56:31 -0700

    urchin kelp timeseries fig

--- analyses/4patch_drivers/FigS1_kelp_urchin_timeseries.R
+++ analyses/4patch_drivers/FigS1_kelp_urchin_timeseries.R
@@ -10,6 +10,7 @@ library(dplyr)
 ################################################################################
 #set directories and load data
 basedir <- "/Volumes/seaotterdb$/kelp_recovery/"
+figdir <- here::here("analyses","4patch_drivers","Figures")
 
 swath_raw <- read.csv(file.path(basedir, "data/subtidal_monitoring/processed/kelp_swath_counts_CC.csv")) %>%
   #select sites in Carmel and Monterey Bay only
@@ -52,19 +53,27 @@ swath_sub <- left_join(swath_sub1, kelp_mean, by=c("year", "site", "outbreak_per
 #plot
 
 # Theme
-my_theme <-  theme(axis.text=element_text(size=7),
+my_theme <-  theme(axis.text=element_text(size=6),
                    axis.text.y = element_text(angle = 90, hjust = 0.5),
                    axis.title=element_text(size=8),
                    plot.tag=element_blank(), #element_text(size=8),
-                   plot.title =element_text(size=8, face="bold"),
-                   # Gridlines
+                   plot.title =element_text(size=7, face="bold"),
+                   # Gridlines 
                    panel.grid.major = element_blank(), 
                    panel.grid.minor = element_blank(),
                    panel.background = element_blank(), 
                    axis.line = element_line(colour = "black"),
                    # Legend
                    legend.key = element_blank(),
-                   legend.background = element_rect(fill=alpha('blue', 0)))
+                   legend.background = element_rect(fill=alpha('blue', 0)),
+                   legend.key.height = unit(1, "lines"), 
+                   legend.text = element_text(size = 6),
+                   legend.title = element_text(size = 7),
+                   #legend.spacing.y = unit(0.75, "cm"),
+                   #facets
+                   strip.background = element_blank(),
+                   strip.text = element_text(size = 6 ,face="bold"),
+                   )
 
 
 
@@ -78,14 +87,39 @@ swath_sub$counts[swath_sub$species == "strongylocentrotus_purpuratus"] <-
 # create a new column for the site factor with levels ordered by kelp_mean
 swath_sub$site_ordered <- reorder(swath_sub$site, -swath_sub$kelp_mean)
 
-ggplot(swath_sub, aes(x = year, y = log(counts), fill = species, color=species, group=species)) +
-  geom_point(aes(color = species), alpha = 0.2) +
+g <- ggplot(swath_sub %>%
+        mutate(species = ifelse(species == "macrocystis_pyrifera","Giant kelp \n(M. pyrifera)","Purple sea urchins \n(S. purpuratus)"),
+               site = gsub("_", " ", site))
+       , aes(x = year, y = log(counts), fill = species, color=species, group=species)) +
+  geom_point(aes(color = species), alpha = 0.2, size=0.5) +
   geom_smooth(method = "auto", se = TRUE, size = 0.5, aes(group = species, color = species), alpha = 0.3, inherit.aes = TRUE) +
-  facet_wrap(~ site, scales = "fixed") +
+  #SSW
+  geom_vline(xintercept = 2013, linetype = "dotted", size=0.3)+
+  annotate(geom="text", label="SSW", x=2010, y=10 , size=2) +
+  annotate("segment", x = 2011.8, y = 9.8, xend = 2012.7, yend = 8.8,
+           arrow = arrow(type = "closed", length = unit(0.02, "npc")))+
+  #define MHW period
+  # Heatwave
+  annotate(geom="rect", xmin=2014, xmax=2016, ymin=-Inf, ymax=Inf, fill="indianred1", alpha=0.2) +
+  annotate(geom="text", label="MHW", x=2018.5, y=10 , size=2) +
+  annotate("segment", x = 2016.5, y = 10, xend = 2015, yend = 10,
+           arrow = arrow(type = "closed", length = unit(0.02, "npc")))+
+  #
+  facet_wrap(~ site, scales = "free") +
   scale_color_manual(values = c("forestgreen", "purple")) +
   scale_fill_manual(values = c("forestgreen", "purple")) +
-  ylim(-3, 10) +
-  my_theme
+  ylim(-0.5, 10) +
+  labs(fill = "Species", color = "Species")+
+  ylab("Log density (No. individuals per 60 m²)")+
+  xlab("Year")+
+  my_theme + theme(legend.position = "top")
+
+# Export figure
+ggsave(g, filename=file.path(figdir, "FigS1_urchin_kelp_timeseries.png"), 
+       width=6.5, height=7, units="in", dpi=600)
+
+
+
 
 
 
