23a9e5e6aa1b0bcf48d24562896db3f794a1163a
Joshua Smith <jossmith@MBA230111.mbayaq.org>
2023-05-04 13:31:19 -0700

    working

--- analyses/4patch_drivers/Code/FigSX_site_diversity.R
+++ analyses/4patch_drivers/Code/FigSX_site_diversity.R
@@ -3,13 +3,14 @@
 #April 28, 2023
 
 rm(list=ls())
-librarian::shelf(tidyverse, here, vegan)
+librarian::shelf(tidyverse, here, vegan, cowplot, ggpubr)
 
 library(dplyr)
 
 ################################################################################
 #set directories and load data
 basedir <- "/Volumes/seaotterdb$/kelp_recovery/"
+figdir <- here::here("analyses","4patch_drivers","Figures")
 
 fish_raw <- read.csv(file.path(basedir, "data/subtidal_monitoring/processed/kelp_fish_counts_CC.csv")) %>%
   #select sites in Carmel and Monterey Bay only
@@ -69,9 +70,10 @@ swath_groups <- swath_raw %>% dplyr::select(1:11)
 swath_richness <- data.frame(S.obs = apply(swath_dat[,1:59]>0, 1, sum))
 swath_evenness <- diversity(swath_dat)/log(specnumber(swath_dat))
 swath_shannon <- diversity(swath_dat, index="shannon")
+swath_simpson <- diversity(swath_dat, index="simpson")
 swath_abund <- rowSums(swath_dat[,1:59])
 
-swath_alphadiv <- cbind(swath_groups, swath_richness, swath_shannon, swath_evenness, swath_abund)%>%
+swath_alphadiv <- cbind(swath_groups, swath_richness, swath_shannon, swath_simpson, swath_evenness, swath_abund)%>%
   mutate(MHW = str_to_sentence(MHW),
          MHW = factor(MHW, levels=c("Before","During","After")))
 
@@ -92,9 +94,10 @@ upc_dat[,1:56] <- abundance
 upc_richness <- data.frame(S.obs = apply(upc_dat[,1:56]>0, 1, sum))
 upc_evenness <- diversity(upc_dat)/log(specnumber(upc_dat))
 upc_shannon <- diversity(upc_dat, index="shannon")
+upc_simpson <- diversity(upc_dat, index="simpson")
 upc_abund <- rowSums(upc_dat[,1:56])
 
-upc_alphadiv <- cbind(upc_groups, upc_richness, upc_shannon, upc_evenness, upc_abund)%>%
+upc_alphadiv <- cbind(upc_groups, upc_richness, upc_shannon, upc_simpson, upc_evenness, upc_abund)%>%
   mutate(MHW = str_to_sentence(MHW),
          MHW = factor(MHW, levels=c("Before","During","After")))
 
@@ -107,15 +110,16 @@ fish_groups <- fish_raw %>% dplyr::select(1:12)
 fish_richness <- data.frame(S.obs = apply(fish_dat[,1:111]>0, 1, sum))
 fish_evenness <- diversity(fish_dat)/log(specnumber(fish_dat))
 fish_shannon <- diversity(fish_dat, index="shannon")
+fish_simpson <- diversity(fish_dat, index="simpson")
 fish_abund <- rowSums(fish_dat[,1:111])
 
-fish_alphadiv <- cbind(fish_groups, fish_richness, fish_shannon, fish_evenness, fish_abund)%>%
+fish_alphadiv <- cbind(fish_groups, fish_richness, fish_shannon, fish_simpson, fish_evenness, fish_abund)%>%
   mutate(MHW = str_to_sentence(MHW),
          MHW = factor(MHW, levels=c("Before","During","After")),
          fish_evenness = ifelse(fish_evenness == "NaN",NA,fish_evenness))
   
 ################################################################################
-###test for significant differences
+###test for significant differences for swath
 
 
 # Load the data
@@ -151,10 +155,124 @@ swath_sig <- results %>%
                      sig = ifelse(p_new < 0.05,"*",""),
                    p_lab = paste0("p < ",p_new,sig),
                    slope = paste("Linear slope:",slope)) %>% distinct()
-                  
 
 ################################################################################
-#plot
+###test for significant differences for fish
+                  
+
+# Load the data
+my_data <- fish_alphadiv
+
+# Get a vector of unique site names
+site_names <- unique(my_data$site)
+
+
+# Create an empty data frame to store the results
+results <- data.frame(site = character(),
+                      intercept = numeric(),
+                      slope = numeric(),
+                      p_value = numeric(),
+                      stringsAsFactors = FALSE)
+
+# Loop over each site and fit a linear regression model
+for (site_name in site_names) {
+  site_data <- subset(my_data, site == site_name)
+  model <- lm(fish_shannon ~ year, data = site_data)
+  p_value <- summary(model)$coefficients[2,4]
+  p_value <- format(p_value, scientific = FALSE)
+  results <- rbind(results, data.frame(site = site_name,
+                                       intercept = coef(model)[1],
+                                       slope = round(coef(model)[2],2),
+                                       p_value = round(as.numeric(p_value),3)))
+}
+
+View(results)
+
+fish_sig <- results %>%
+  mutate(p_new = ifelse(p_value == 0 ,0.001,p_value),
+         sig = ifelse(p_new < 0.05,"*",""),
+         p_lab = paste0("p < ",p_new,sig),
+         slope = paste("Linear slope:",slope)) %>% distinct()
+
+################################################################################
+###test for significant differences for UPC
+
+
+# Load the data
+my_data <- upc_alphadiv
+
+# Get a vector of unique site names
+site_names <- unique(my_data$site)
+
+
+# Create an empty data frame to store the results
+results <- data.frame(site = character(),
+                      intercept = numeric(),
+                      slope = numeric(),
+                      p_value = numeric(),
+                      stringsAsFactors = FALSE)
+
+# Loop over each site and fit a linear regression model
+for (site_name in site_names) {
+  site_data <- subset(my_data, site == site_name)
+  model <- lm(upc_shannon ~ year, data = site_data)
+  p_value <- summary(model)$coefficients[2,4]
+  p_value <- format(p_value, scientific = FALSE)
+  results <- rbind(results, data.frame(site = site_name,
+                                       intercept = coef(model)[1],
+                                       slope = round(coef(model)[2],2),
+                                       p_value = round(as.numeric(p_value),3)))
+}
+
+View(results)
+
+upc_sig <- results %>%
+  mutate(p_new = ifelse(p_value == 0 ,0.001,p_value),
+         sig = ifelse(p_new < 0.05,"*",""),
+         p_lab = paste0("p < ",p_new,sig),
+         slope = paste("Linear slope:",slope)) %>% distinct()
+
+################################################################################
+###test for significant differences overall
+
+#swath
+  model <- lm(swath_shannon ~ year, data = swath_alphadiv)
+  p_value <- summary(model)$coefficients[2,4]
+  p_value <- format(p_value, scientific = FALSE)
+  swath_results <- cbind(data.frame(intercept = coef(model)[1],
+                                       slope = round(coef(model)[2],2),
+                                       p_value = round(as.numeric(p_value),3))) %>%
+        mutate(p_new = ifelse(p_value == 0 ,0.001,p_value),
+         sig = ifelse(p_new < 0.05,"*",""),
+         p_lab = paste0("p < ",p_new,sig),
+         slope = paste("Linear slope:",slope)) %>% distinct()
+  
+#Fish
+  model <- lm(fish_shannon ~ year, data = fish_alphadiv)
+  p_value <- summary(model)$coefficients[2,4]
+  p_value <- format(p_value, scientific = FALSE)
+  fish_results <- cbind(data.frame(intercept = coef(model)[1],
+                                    slope = round(coef(model)[2],2),
+                                    p_value = round(as.numeric(p_value),3))) %>%
+    mutate(p_new = ifelse(p_value == 0 ,0.001,p_value),
+           sig = ifelse(p_new < 0.05,"*",""),
+           p_lab = paste0("p < ",p_new,sig),
+           slope = paste("Linear slope:",slope)) %>% distinct()
+
+  #UPC
+  model <- lm(upc_shannon ~ year, data = upc_alphadiv)
+  p_value <- summary(model)$coefficients[2,4]
+  p_value <- format(p_value, scientific = FALSE)
+  upc_results <- cbind(data.frame(intercept = coef(model)[1],
+                                   slope = round(coef(model)[2],2),
+                                   p_value = round(as.numeric(p_value),3))) %>%
+    mutate(p_new = ifelse(p_value == 0 ,0.001,p_value),
+           sig = ifelse(p_new < 0.05,"*",""),
+           p_lab = paste0("p < ",p_new,sig),
+           slope = paste("Linear slope:",slope)) %>% distinct()
+
+################################################################################
+#plot site level
 
 # Theme
 my_theme <-  theme(axis.text=element_text(size=6),
@@ -179,24 +297,6 @@ my_theme <-  theme(axis.text=element_text(size=6),
                    strip.text = element_text(size = 6 ,face="bold"),
 )
 
-overall <- ggplot(swath_alphadiv, aes(x = year, y=swath_shannon))+
-  geom_point(alpha = 0.2, size=0.5) +
-  geom_smooth(method = "auto", se = TRUE, size = 0.5, alpha = 0.3, inherit.aes = TRUE) +
-  theme_bw() + my_theme
-
-overall
-
-
-swath_plot <- ggplot(swath_alphadiv, aes(x = year, y=swath_shannon))+
-              geom_point(alpha = 0.2, size=0.5) +
-              geom_smooth(method = "auto", se = TRUE, size = 0.5, alpha = 0.3, inherit.aes = TRUE) +
-              facet_wrap(~site, scales="free_x")+ theme_bw() + my_theme
-              
-
-swath_plot
-
-
-
 
 all_three <- ggplot()+
   #swath
@@ -209,10 +309,14 @@ all_three <- ggplot()+
   geom_point(aes(x = year-0.1, y=fish_shannon, color = "Fish"), alpha = 0.2, size=0.5, data = fish_alphadiv %>% mutate(site = gsub("_", " ", site)), 
              position = position_jitter(width = 0.02)) +
   geom_smooth(method = "auto", se = TRUE, size = 0.5, alpha = 0.3, aes(x = year, y=fish_shannon, color = "Fish"),data = fish_alphadiv %>% mutate(site = gsub("_", " ", site)) ) +
+  geom_text(aes(x=2018, y=2.6, label=p_lab, color="Fish"), size=3, data=fish_sig %>% mutate(site = gsub("_", " ", site)))+ #add p
+  geom_text(aes(x=2014, y=2.6, label=slope, color="Fish"), size=3, data=fish_sig %>% mutate(site = gsub("_", " ", site)))+ #add slope
   #upc
   geom_point(aes(x = year+0.1, y=upc_shannon, color = "UPC"), alpha = 0.2, size=0.5, data = upc_alphadiv %>% mutate(site = gsub("_", " ", site)), 
              position = position_jitter(width = 0.02)) +
   geom_smooth(method = "auto", se = TRUE, size = 0.5, alpha = 0.3, aes(x = year, y=upc_shannon, color = "UPC"),data = upc_alphadiv %>% mutate(site = gsub("_", " ", site))) +
+  geom_text(aes(x=2018, y=2.4, label=p_lab, color="UPC"), size=3, data=upc_sig %>% mutate(site = gsub("_", " ", site)))+ #add p
+  geom_text(aes(x=2014, y=2.4, label=slope, color="UPC"), size=3, data=upc_sig %>% mutate(site = gsub("_", " ", site)))+ #add slope
   facet_wrap(~site)+
   #color
   scale_color_manual(values = c("#009E73", "#E69F00","#CC79A7"), labels = c("Fish", "Swath","UPC"))+ 
@@ -221,9 +325,142 @@ all_three <- ggplot()+
 all_three
 
 
+################################################################################
+#plot overall
+
+# Theme
+my_theme <-  theme(axis.text=element_text(size=6),
+                   axis.text.y = element_text(angle = 90, hjust = 0.5),
+                   axis.title=element_text(size=8),
+                   plot.tag=element_blank(), #element_text(size=8),
+                   plot.title =element_text(size=7, face="bold"),
+                   # Gridlines 
+                   panel.grid.major = element_blank(), 
+                   panel.grid.minor = element_blank(),
+                   panel.background = element_blank(), 
+                   axis.line = element_line(colour = "black"),
+                   # Legend
+                   legend.key = element_blank(),
+                   legend.background = element_rect(fill=alpha('blue', 0)),
+                   legend.key.height = unit(1, "lines"), 
+                   legend.text = element_text(size = 6),
+                   legend.title = element_text(size = 7),
+                   #legend.spacing.y = unit(0.75, "cm"),
+                   #facets
+                   strip.background = element_blank(),
+                   strip.text = element_text(size = 6 ,face="bold"),
+)
+
+
+shannon <- ggplot()+
+  #swath
+  geom_point(aes(x = year, y=swath_shannon, color = "Swath", fill="Swath"), alpha = 0.05, size=0.5, data = swath_alphadiv %>% mutate(site = gsub("_", " ", site)),
+             position = position_jitter(width = 0.1)) +
+  geom_smooth(method = "auto", se = TRUE, size = 0.5, alpha = 0.3, aes(x = year, y=swath_shannon, color = "Swath", fill="Swath"),data = swath_alphadiv %>% mutate(site = gsub("_", " ", site))) +
+  #fish
+  geom_point(aes(x = year-0.1, y=fish_shannon, color = "Fish", fill="Fish"), alpha = 0.05, size=0.5, data = fish_alphadiv %>% mutate(site = gsub("_", " ", site)), 
+             position = position_jitter(width = 0.1)) +
+  geom_smooth(method = "auto", se = TRUE, size = 0.5, alpha = 0.3, aes(x = year, y=fish_shannon, color = "Fish", fill="Fish"),data = fish_alphadiv %>% mutate(site = gsub("_", " ", site)) ) +
+  #upc
+  geom_point(aes(x = year+0.1, y=upc_shannon, color = "UPC",fill="UPC"), alpha = 0.05, size=0.5, data = upc_alphadiv %>% mutate(site = gsub("_", " ", site)), 
+             position = position_jitter(width = 0.1)) +
+  geom_smooth(method = "auto", se = TRUE, size = 0.5, alpha = 0.3, aes(x = year, y=upc_shannon, color = "UPC", fill = "UPC"),data = upc_alphadiv %>% mutate(site = gsub("_", " ", site))) +
+  #color
+  scale_color_manual(values = c("#009E73", "#E69F00","#CC79A7"), labels = c("Fish", "Swath","UPC"))+ 
+  scale_fill_manual(values = c("#009E73", "#E69F00","#CC79A7"), labels = c("Fish", "Swath","UPC"))+ 
+  theme_bw() + my_theme +
+  labs(color = 'Survey method', fill = "Survey method")+
+  ylab("Shannon diversity")+
+  xlab("Year")+
+  ggtitle("Shannon diversity")
+
+shannon
+
+simpson <- ggplot()+
+  #swath
+  geom_point(aes(x = year, y=swath_simpson, color = "Swath", fill="Swath"), alpha = 0.05, size=0.5, data = swath_alphadiv %>% mutate(site = gsub("_", " ", site)),
+             position = position_jitter(width = 0.1)) +
+  geom_smooth(method = "auto", se = TRUE, size = 0.5, alpha = 0.3, aes(x = year, y=swath_simpson, color = "Swath", fill="Swath"),data = swath_alphadiv %>% mutate(site = gsub("_", " ", site))) +
+  #fish
+  geom_point(aes(x = year-0.1, y=fish_simpson, color = "Fish", fill="Fish"), alpha = 0.05, size=0.5, data = fish_alphadiv %>% mutate(site = gsub("_", " ", site)), 
+             position = position_jitter(width = 0.1)) +
+  geom_smooth(method = "auto", se = TRUE, size = 0.5, alpha = 0.3, aes(x = year, y=fish_simpson, color = "Fish", fill="Fish"),data = fish_alphadiv %>% mutate(site = gsub("_", " ", site)) ) +
+  #upc
+  geom_point(aes(x = year+0.1, y=upc_simpson, color = "UPC",fill="UPC"), alpha = 0.05, size=0.5, data = upc_alphadiv %>% mutate(site = gsub("_", " ", site)), 
+             position = position_jitter(width = 0.1)) +
+  geom_smooth(method = "auto", se = TRUE, size = 0.5, alpha = 0.3, aes(x = year, y=upc_simpson, color = "UPC", fill = "UPC"),data = upc_alphadiv %>% mutate(site = gsub("_", " ", site))) +
+  #color
+  scale_color_manual(values = c("#009E73", "#E69F00","#CC79A7"), labels = c("Fish", "Swath","UPC"))+ 
+  scale_fill_manual(values = c("#009E73", "#E69F00","#CC79A7"), labels = c("Fish", "Swath","UPC"))+ 
+  theme_bw() + my_theme +
+  labs(color = 'Survey method', fill = "Survey method")+
+  ylab("Simpson diversity")+
+  xlab("Year")+
+  ggtitle("Simpson diversity")
+
+simpson
+
+
+richness <- ggplot()+
+  #swath
+  geom_point(aes(x = year, y=S.obs, color = "Swath", fill="Swath"), alpha = 0.05, size=0.5, data = swath_alphadiv %>% mutate(site = gsub("_", " ", site)),
+             position = position_jitter(width = 0.1)) +
+  geom_smooth(method = "auto", se = TRUE, size = 0.5, alpha = 0.3, aes(x = year, y=S.obs, color = "Swath", fill="Swath"),data = swath_alphadiv %>% mutate(site = gsub("_", " ", site))) +
+  #fish
+  geom_point(aes(x = year-0.1, y=S.obs, color = "Fish", fill="Fish"), alpha = 0.05, size=0.5, data = fish_alphadiv %>% mutate(site = gsub("_", " ", site)), 
+             position = position_jitter(width = 0.1)) +
+  geom_smooth(method = "auto", se = TRUE, size = 0.5, alpha = 0.3, aes(x = year, y=S.obs, color = "Fish", fill="Fish"),data = fish_alphadiv %>% mutate(site = gsub("_", " ", site)) ) +
+  #upc
+  geom_point(aes(x = year+0.1, y=S.obs, color = "UPC",fill="UPC"), alpha = 0.05, size=0.5, data = upc_alphadiv %>% mutate(site = gsub("_", " ", site)), 
+             position = position_jitter(width = 0.1)) +
+  geom_smooth(method = "auto", se = TRUE, size = 0.5, alpha = 0.3, aes(x = year, y=S.obs, color = "UPC", fill = "UPC"),data = upc_alphadiv %>% mutate(site = gsub("_", " ", site))) +
+  #color
+  scale_color_manual(values = c("#009E73", "#E69F00","#CC79A7"), labels = c("Fish", "Swath","UPC"))+ 
+  scale_fill_manual(values = c("#009E73", "#E69F00","#CC79A7"), labels = c("Fish", "Swath","UPC"))+ 
+  theme_bw() + my_theme +
+  labs(color = 'Survey method', fill = "Survey method")+
+  ylab("Species richness (n)")+
+  xlab("Year")+
+  ggtitle("Species richness")
+
+richness
 
 
 
+evenness <- ggplot()+
+  #swath
+  geom_point(aes(x = year, y=swath_evenness, color = "Swath", fill="Swath"), alpha = 0.05, size=0.5, data = swath_alphadiv %>% mutate(site = gsub("_", " ", site)),
+             position = position_jitter(width = 0.1)) +
+  geom_smooth(method = "auto", se = TRUE, size = 0.5, alpha = 0.3, aes(x = year, y=swath_evenness, color = "Swath", fill="Swath"),data = swath_alphadiv %>% mutate(site = gsub("_", " ", site))) +
+  #fish
+  geom_point(aes(x = year-0.1, y=fish_evenness, color = "Fish", fill="Fish"), alpha = 0.05, size=0.5, data = fish_alphadiv %>% mutate(site = gsub("_", " ", site)), 
+             position = position_jitter(width = 0.1)) +
+  geom_smooth(method = "auto", se = TRUE, size = 0.5, alpha = 0.3, aes(x = year, y=fish_evenness, color = "Fish", fill="Fish"),data = fish_alphadiv %>% mutate(site = gsub("_", " ", site)) ) +
+  #upc
+  geom_point(aes(x = year+0.1, y=upc_evenness, color = "UPC",fill="UPC"), alpha = 0.05, size=0.5, data = upc_alphadiv %>% mutate(site = gsub("_", " ", site)), 
+             position = position_jitter(width = 0.1)) +
+  geom_smooth(method = "auto", se = TRUE, size = 0.5, alpha = 0.3, aes(x = year, y=upc_evenness, color = "UPC", fill = "UPC"),data = upc_alphadiv %>% mutate(site = gsub("_", " ", site))) +
+  #color
+  scale_color_manual(values = c("#009E73", "#E69F00","#CC79A7"), labels = c("Fish", "Swath","UPC"))+ 
+  scale_fill_manual(values = c("#009E73", "#E69F00","#CC79A7"), labels = c("Fish", "Swath","UPC"))+ 
+  theme_bw() + my_theme +
+  labs(color = 'Survey method', fill = "Survey method")+
+  ylab("Species evenness")+
+  xlab("Year")+
+  ggtitle("Species evenness")
+
+evenness
+
+
+# Combine ggplots with ggarrange()
+combined_plot <- ggarrange(shannon, simpson, richness, evenness, ncol = 2, nrow=2, common.legend = TRUE, legend = "right")
+# Display the plot
+combined_plot
+
+
+
+ggsave(combined_plot, filename=file.path(figdir, "Fig2_diversity_overall.png"), bg = "white",
+       width=6.5, height=6, units="in", dpi=600)
 
 
 
