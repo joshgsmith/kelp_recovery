188faefc4f50172c21d52afd351e8df72d7f1528
Joshua Smith <joshsmith@nceas.ucsb.edu>
2023-05-15 10:29:26 -0700

    finished with substrate

--- data/subtidal_monitoring/step3_build_predictors.R
+++ data/subtidal_monitoring/step3_build_predictors.R
@@ -14,6 +14,10 @@ vrm_raw <- readxl::read_excel(file.path(basedir,"data/seafloor_data/processed/vr
 slope_raw  <- readxl::read_excel(file.path(basedir,"data/seafloor_data/processed/slope_pisco_sites.xls"))
 bat_raw <- readxl::read_excel(file.path(basedir,"data/seafloor_data/processed/bathy_pisco_sites.xls"))
 
+#load environmental data
+cuti_beuti_raw <- readRDS(file.path(basedir, "/data/environmental_data/CUTI/processed/1988_2022_cuti_beuti_daily_by_PISCO_site.Rds"))
+sst_raw <- readRDS(file.path(basedir, "/data/environmental_data/MURSST/processed/2002_2022_mursst_monthly_by_PISCO_site.Rds"))
+
 ################################################################################
 #load sites
 
@@ -36,15 +40,80 @@ site_table <- read.csv(file.path(basedir, "data/subtidal_monitoring/raw/MLPA_kel
                     site == "PT_JOE" |
                     site == "SPANISH_BAY_DC" |
                     site == "SPANISH_BAY_UC" |
-                    site == "BIRD_ROCK"))%>%
+                    site == "BIRD_ROCK"|
+                    site == "LINGCOD_DC"|
+                    site == "LINGCOD_UC"|
+                    site == "CARMEL_DC"|
+                    site == "CARMEL_UC"))%>%
   distinct() #remove duplicates
 
 
 ################################################################################
-#process VRM
+#process seafloor data
+
+vrm_build1 <- vrm_raw %>% dplyr::select(site = SITE_SIDE, vrm_mean = MEAN, 
+                                        vrm_range = RANGE, vrm_sum = SUM)%>%
+                #rename sites to match
+  mutate(site = ifelse(site == "LONE_TREE_CEN","LONE_TREE", site),
+         site = ifelse(site == "SIREN_CEN","SIREN", site))
+
+slope_build1 <- slope_raw %>% dplyr::select(site = SITE_SIDE, slope_mean = MEAN, 
+                                        slope_range = RANGE, slope_sum = SUM)%>%
+  #rename sites to match
+  mutate(site = ifelse(site == "LONE_TREE_CEN","LONE_TREE", site),
+         site = ifelse(site == "SIREN_CEN","SIREN", site))
+
+bat_build1 <- bat_raw %>% dplyr::select(site = SITE_SIDE, bat_mean = MEAN, 
+                                            bat_range = RANGE) %>%
+                        #make positive-definite
+                        mutate(bat_mean = bat_mean *-1)%>%
+  #rename sites to match
+  mutate(site = ifelse(site == "LONE_TREE_CEN","LONE_TREE", site),
+         site = ifelse(site == "SIREN_CEN","SIREN", site))
 
 
+#join seafloor with site table
 
+site_predict_build1 <- site_table %>% left_join(vrm_build1, by="site") %>%
+                        left_join(slope_build1, by="site")%>%
+                        left_join(bat_build1, by="site")
+
+
+################################################################################
+#process environmental data
+
+cuti_beuti_build1 <- cuti_beuti_raw %>% dplyr::select(site, year, month, date, cuti, beuti) %>%
+  #calculate annual means at each site
+  group_by(site, year) %>%
+  dplyr::summarize(cuti_avg = mean(cuti),
+                   beuti_avg = mean(beuti))
+
+sst_build1 <- sst_raw %>% 
+  dplyr::mutate(year = lubridate::year(date),
+                month = lubridate::month(date),
+                day = lubridate::day(date))%>%
+  dplyr::select(year, month, day, site, sst_c)%>%
+  #calculate annial means at each site
+  group_by(year, site)%>%
+  dplyr::summarize(sst_c_mean = mean(sst_c))
+
+################################################################################
+#join sampling data with environmental
+
+mod_dat_build1 <- left_join(swath_raw, cuti_beuti_build1, by = c("year", "site")) %>%
+  dplyr::select(year, beuti_avg, cuti_avg, everything())
+
+mod_dat_build2 <- left_join(mod_dat_build1, sst_build1, by = c("year", "site")) %>%
+  dplyr::select(year, sst_c_mean, everything())
+
+# create lagged variables
+mod_dat_build3 <- mod_dat_build2 %>%
+  mutate(lag_beuti_1 = lag(beuti_avg, 1),
+         lag_cuti_1 = lag(cuti_avg, 1),
+         lag_sst_1 = lag(sst_c_mean, 1),
+         lag_beuti_2 = lag(beuti_avg, 2),
+         lag_cuti_2 = lag(cuti_avg, 2),
+         lag_sst_2 = lag(sst_c_mean, 2))
 
 
 
