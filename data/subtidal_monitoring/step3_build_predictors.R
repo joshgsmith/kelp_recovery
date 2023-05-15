87bb864e8c0b9ced9539d641388f38a2d3b9c9f6
Joshua Smith <joshsmith@nceas.ucsb.edu>
2023-05-15 11:31:40 -0700

    build predictors

--- data/subtidal_monitoring/step3_build_predictors.R
+++ data/subtidal_monitoring/step3_build_predictors.R
@@ -15,8 +15,7 @@ slope_raw  <- readxl::read_excel(file.path(basedir,"data/seafloor_data/processed
 bat_raw <- readxl::read_excel(file.path(basedir,"data/seafloor_data/processed/bathy_pisco_sites.xls"))
 
 #load environmental data
-cuti_beuti_raw <- readRDS(file.path(basedir, "/data/environmental_data/CUTI/processed/1988_2022_cuti_beuti_daily_by_PISCO_site.Rds"))
-sst_raw <- readRDS(file.path(basedir, "/data/environmental_data/MURSST/processed/2002_2022_mursst_monthly_by_PISCO_site.Rds"))
+envr_raw <- readRDS( file.path(basedir, "/data/environmental_data/cuti/processed/beuti_cuti_sst_monthly_amoms_by_PISCO_site.Rds"))
 
 ################################################################################
 #load sites
@@ -82,38 +81,40 @@ site_predict_build1 <- site_table %>% left_join(vrm_build1, by="site") %>%
 ################################################################################
 #process environmental data
 
-cuti_beuti_build1 <- cuti_beuti_raw %>% dplyr::select(site, year, month, date, cuti, beuti) %>%
-  #calculate annual means at each site
-  group_by(site, year) %>%
-  dplyr::summarize(cuti_avg = mean(cuti),
-                   beuti_avg = mean(beuti))
-
-sst_build1 <- sst_raw %>% 
-  dplyr::mutate(year = lubridate::year(date),
-                month = lubridate::month(date),
-                day = lubridate::day(date))%>%
-  dplyr::select(year, month, day, site, sst_c)%>%
-  #calculate annial means at each site
-  group_by(year, site)%>%
-  dplyr::summarize(sst_c_mean = mean(sst_c))
+envr_build1 <- envr_raw %>% #select sites in Carmel and Monterey Bay only
+  left_join(site_table, by="site")%>%
+  dplyr::filter(latitude >= 36.46575 & latitude <= 36.64045) %>%
+  #drop sites with insufficient data
+  dplyr::filter(!(site == "ASILOMAR_DC" |
+                    site == "ASILOMAR_UC" |
+                    site == "CHINA_ROCK" |
+                    site == "CYPRESS_PT_DC" |
+                    site == "CYPRESS_PT_UC" |
+                    site == "PINNACLES_IN" |
+                    site == "PINNACLES_OUT" |
+                    site == "PT_JOE" |
+                    site == "SPANISH_BAY_DC" |
+                    site == "SPANISH_BAY_UC" |
+                    site == "BIRD_ROCK"|
+                    site == "LINGCOD_DC"|
+                    site == "LINGCOD_UC"|
+                    site == "CARMEL_DC"|
+                    site == "CARMEL_UC"))%>%
+  dplyr::select(!(c(latitude, longitude, ca_mpa_name_short, mpa_class, mpa_designation, baseline_region)))
 
 ################################################################################
-#join sampling data with environmental
+#join substrate data with envr data
 
-mod_dat_build1 <- left_join(swath_raw, cuti_beuti_build1, by = c("year", "site")) %>%
-  dplyr::select(year, beuti_avg, cuti_avg, everything())
+site_predict_build2 <- left_join(envr_build1, site_predict_build1, by="site") %>%
+                      dplyr::select(year, month, baseline_region, site, latitude, longitude,
+                                    ca_mpa_name_short, mpa_class, mpa_designation, everything())
 
-mod_dat_build2 <- left_join(mod_dat_build1, sst_build1, by = c("year", "site")) %>%
-  dplyr::select(year, sst_c_mean, everything())
 
-# create lagged variables
-mod_dat_build3 <- mod_dat_build2 %>%
-  mutate(lag_beuti_1 = lag(beuti_avg, 1),
-         lag_cuti_1 = lag(cuti_avg, 1),
-         lag_sst_1 = lag(sst_c_mean, 1),
-         lag_beuti_2 = lag(beuti_avg, 2),
-         lag_cuti_2 = lag(cuti_avg, 2),
-         lag_sst_2 = lag(sst_c_mean, 2))
+
+
+
+
+
 
 
 
