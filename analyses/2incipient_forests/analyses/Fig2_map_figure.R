b523d011d623d3f64ed1ddfd561522276a08f42f
Joshua Smith <joshsmith@nceas.ucsb.edu>
2023-08-23 15:16:37 -0700

    working

---
+++ analyses/2incipient_forests/analyses/Fig2_map_figure.R
@@ -0,0 +1,236 @@
+
+
+########
+########
+
+#FIGURE OF SINGLE MAP OF SITES WITH INCIPIENT FORESTS IDENTIFIED
+
+########
+########
+
+#Joshua G. Smith; jossmith@mbayaq.org
+
+rm(list=ls())
+
+
+######
+#required packages
+librarian::shelf(tidyverse, sf, raster, shiny, tmap, terra, tidyterra, ggnewscale)
+
+#set directories 
+basedir <- "/Volumes/seaotterdb$/kelp_recovery/data"
+figdir <- here::here("analyses","2incipient_forests","figures")
+output <- here::here("analyses","2incipient_forests","output")
+
+#read landsat averages with scan area
+scan_orig <- st_read(file.path(output, "landsat_scan_area.geojson"))
+
+#read otter scan area
+scan_area <- st_read(file.path(basedir, "gis_data/raw/otter_scan_area/TrackingMaps_POLY.shp")) 
+
+#read state
+ca_counties_orig <- st_read(file.path(basedir, "gis_data/raw/ca_county_boundaries/s7vc7n.shp")) 
+
+#read landsat raw
+landsat_orig <- st_read(file.path(basedir, "kelp_landsat/processed/monterey_peninsula/landsat_mpen_1984_2023_points_withNAs.shp"))
+
+#read foraging data
+forage_orig <- read.csv(file.path(basedir, "foraging_data/processed/foraging_data_2016_2023.csv")) 
+
+
+################################################################################
+#Step 1 - raster for focal year and quarter
+
+# transform landsat data to Teale Albers
+rast_build1 <- st_transform(landsat_orig, crs = 3310) %>% 
+  mutate(biomass = ifelse(biomass == 0, NA, biomass))  %>% filter (year == 2022 & quarter ==3) 
+
+#define blank raster
+r <- rast(rast_build1, res=30)
+
+landsat_rast_2022 <- rasterize(rast_build1, r, field = "biomass", fun = mean)
+
+plot(landsat_rast)
+
+
+rast_build2 <- st_transform(landsat_orig, crs = 3310) %>% 
+  mutate(biomass = ifelse(biomass == 0, NA, biomass))  %>% filter (year == 2017 & quarter ==3) 
+landsat_rast_2017 <- rasterize(rast_build2, r, field = "biomass", fun = mean)
+
+################################################################################
+#Step 2 - define max kelp extent
+
+#select MPEN as focal region 
+plot_dat <- rast_build1 %>% filter (latitude >= 36.510140 &
+                                       latitude <= 36.670574) %>% st_transform(crs=3310)
+
+#define 0 cover as historical kelp footprint
+na_dat <- landsat_orig %>% filter (latitude >= 36.510140 &
+                                    latitude <= 36.670574, 
+                                  biomass == 0)
+
+#transform landsat data to Teale Albers
+t_dat <- st_transform(plot_dat, crs=3310) %>% 
+  mutate(biomass = ifelse(biomass==0,NA,biomass))
+
+kelp_historic <- st_transform(na_dat, crs=3310) # %>% mutate(biomass = ifelse(biomass==0,1,NA))
+
+#create grid
+r <- rast(t_dat, res=30)  # Builds a blank raster of given dimensions and resolution  
+vr <- rasterize(t_dat, r,"biomass", resolution = 30) 
+
+kelp_na <- rasterize(kelp_historic, r,"biomass", resolution = 30) 
+
+plot(kelp_na)
+
+################################################################################
+#Step 3 - process scan area
+
+scan_plot_area <- scan_area %>% mutate(Name = factor(Name)) %>%
+  rename(site_name = Name)%>%
+  mutate(Incipient = ifelse(site_name == "3200"|
+                              site_name == "Carmel River Beach"|
+                              site_name == "Coast Guard Pier" |
+                              site_name == "El Torito" |
+                              site_name == "Hopkins Marine Station East"|
+                              site_name == "Lone Cypress" |
+                              site_name =="Monastery" |
+                              site_name == "Monterey Bay Inn" |
+                              site_name == "Pescadero Point East" |
+                              site_name == "Pescadero Point West"|
+                              site_name == "Whaler's Cove","Yes","No"),
+         incipient = factor(Incipient, levels = c("Yes","No")))
+
+
+
+################################################################################
+#Step 4 - plot
+
+# Theme
+base_theme <-  theme(axis.text=element_text(size=6, color = "black"),
+                     axis.title=element_text(size=7,color = "black"),
+                     plot.tag=element_text(size=7,color = "black"),
+                     plot.title=element_text(size=7,color = "black", face = "bold"),
+                     # Gridlines
+                     panel.grid.major = element_blank(), 
+                     panel.grid.minor = element_blank(),
+                     panel.background = element_blank(), 
+                     axis.line = element_line(colour = "black"),
+                     # Legend
+                     legend.key.size = unit(0.3, "cm"), 
+                     legend.key = element_rect(fill=alpha('blue', 0)),
+                     legend.spacing.y = unit(0.1, "cm"),  
+                     legend.text=element_text(size=5,color = "black"),
+                     legend.title=element_text(size=6,color = "black"),
+                     #legend.key.height = unit(0.1, "cm"),
+                     legend.background = element_rect(fill=alpha('blue', 0)),
+                     #facets
+                     strip.text = element_text(size=6, face = "bold",color = "black", hjust=0),
+                     strip.background = element_blank())
+
+
+ 
+p1 <- ggplot() +
+  geom_sf(data = scan_plot_area, aes(fill = Incipient), color = NA,
+          alpha = 0.4) +
+  scale_fill_manual(values = c("lightpink", "transparent"),
+                    breaks = c("Yes", "No"),
+                    labels = c("", "")) +
+  labs(fill = "Incipient \nforest")+
+  ggnewscale::new_scale_fill()+
+  #add historic kelp extent
+  tidyterra::geom_spatraster(data = kelp_na, na.rm = TRUE) +
+  tidyterra::scale_fill_hypso_c(
+    palette = "moon_bathy",
+    na.value = NA,
+    labels = NULL
+  ) +
+  guides(fill = guide_legend(override.aes = list(size = 3))) +
+  labs(fill = "Max kelp \nextent")+
+  #add observed landsat for 2022 Q3
+  ggnewscale::new_scale_fill()+
+  tidyterra::geom_spatraster(data = landsat_rast_2022, na.rm = TRUE) +
+  tidyterra::scale_fill_whitebox_c(
+    palette = "atlas",
+    na.value = NA
+  ) +
+  labs(fill = expression("Kelp biomass" ~(kg~"/"~"30m"^{-2})))+
+  #labs(fill = expression(atop("Kelp biomass", ~(kg~"per"~"30m"^{-2}))))+
+  #add scan area
+  ggnewscale::new_scale_fill() +  # Start a new fill scale
+  geom_sf(data = scan_plot_area, fill = NA, color = "gray20") +
+  #add land
+  geom_sf(data = ca_counties_orig, fill = "gray", color = "gray80") +
+  coord_sf(xlim = c(-121.99, -121.88), ylim = c(36.519, 36.645), crs = 4326)+
+  labs(title = "2022", tag = "B")+
+  #reorder legend
+  guides(
+    fill = guide_legend(order = 2),  
+    fill_new_scale = guide_legend(order = 3),  
+    fill_new_scale1 = guide_legend(order = 1)  
+  ) +  theme_bw() + base_theme +theme(axis.text.y = element_blank(),
+                                      plot.tag.position = c(-0.03, 1))
+p1
+
+
+p2 <- ggplot() +
+  #add historic kelp extent
+  tidyterra::geom_spatraster(data = kelp_na, na.rm = TRUE) +
+  tidyterra::scale_fill_hypso_c(
+    palette = "moon_bathy",
+    na.value = NA,
+    labels = NULL
+  ) +
+  guides(fill = guide_legend(override.aes = list(size = 3))) +
+  labs(fill = "Max kelp \nextent")+
+  #add observed landsat for 2022 Q3
+  ggnewscale::new_scale_fill()+
+  tidyterra::geom_spatraster(data = landsat_rast_2017, na.rm = TRUE) +
+  tidyterra::scale_fill_whitebox_c(
+    palette = "atlas",
+    na.value = NA
+  ) +
+  labs(fill = expression(atop("Kelp biomass", ~(kg~"per"~"30m"^{-2}))))+
+  #add scan area
+  ggnewscale::new_scale_fill() +  # Start a new fill scale
+  geom_sf(data = scan_plot_area, fill = NA, color = "gray20") +
+  #add land
+  geom_sf(data = ca_counties_orig, fill = "gray", color = "gray80") +
+  coord_sf(xlim = c(-121.99, -121.88), ylim = c(36.519, 36.645), crs = 4326)+
+  labs(title = "2017", tag = "A")+
+  theme_bw() + base_theme + theme(legend.position = "none", plot.tag.position = c(0.05, 1))
+p2
+
+
+p <- gridExtra::grid.arrange(p2, p1, nrow=1, widths = c(0.436,0.574))
+
+
+# Export figure
+ggsave(p, filename=file.path(figdir, "Fig2_map_figure.png"), 
+       width=7, height=5, units="in", dpi=600)
+
+
+
+
+
+
+
+# Calculate mean of deviation for each year and Incipient category
+plot_data <- scan_orig %>%
+  group_by(year, Incipient) %>%
+  summarise(mean_deviation = mean(deviation))
+
+# Create the plot
+p3 <- ggplot(scan_orig, aes(x = year, y = deviation, color = Incipient)) +
+  geom_line(data = plot_data, aes(x = year, y = mean_deviation, color = Incipient), size = 1.5, show.legend = FALSE) +
+  geom_point(alpha=0.2) +
+  labs(x = "Year", y = "Mean Standard Deviation from Baseline",
+       title = "Mean Standard Deviation from Baseline by Year and Incipient",
+       color = "Incipient") +
+  theme_minimal()
+
+p3
+
+
+
+
