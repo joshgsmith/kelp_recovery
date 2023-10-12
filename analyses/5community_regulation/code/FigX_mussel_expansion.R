11ec634231fe554160e5b930371bf6cbc23be7d7
Joshua Smith <joshsmith@nceas.ucsb.edu>
2023-10-12 10:12:40 -0700

    working

--- analyses/5community_regulation/code/FigX_mussel_expansion.R
+++ analyses/5community_regulation/code/FigX_mussel_expansion.R
@@ -64,30 +64,58 @@ heatmap_data <- mus_pos_build1 %>%
   group_by(intertidal_sitename, transect, location) %>%
   summarize(
     location_range = max(location) - min(location),
-    Period = factor(!any(ssw_period == "before"), levels = c(TRUE, FALSE), labels = c("After", "Before"))
+    Period = factor(!any(ssw_period == "before"), levels = c(TRUE, FALSE), labels = c("Post-SSW", "Pre-SSW"))
   ) %>%
   arrange(intertidal_sitename, transect) %>%
   group_by(intertidal_sitename) %>%
   mutate(transect_seq = dense_rank(transect),
-         intertidal_sitename = factor(intertidal_sitename,levels = c("Hopkins","Point Pinos","Stillwater","Point Lobos")))
+         intertidal_sitename = factor(intertidal_sitename,levels = c("Hopkins","Point Pinos","Stillwater","Point Lobos")),
+         Period = factor(Period, levels = c("Pre-SSW","Post-SSW")))
 
 
 # Create a heatmap with equal tile size and color points observed only in 'after' differently
 p <- ggplot(heatmap_data, aes(x = transect_seq, y = location, fill = Period)) +
   geom_tile(width=1, height=1) +  # Set width and height to 1 for equal tile size
-  scale_fill_manual(values = c("Before" = "navyblue", "After" = "indianred")) +
+  scale_fill_manual(values = c("Pre-SSW" = "navyblue", "Post-SSW" = "indianred")) +
   labs(title = "",
        x = "Transect number",
-       y = "Distance from high to \nlow intertidal (meters from baseline)") +
+       y = "Distance from high to \nlow intertidal (meters from baseline)",
+       fill = NULL) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   scale_y_reverse()+  # Flip the y-axis
   #ggforce::facet_col(vars(intertidal_sitename), scales = "free") +
   facet_grid(.~intertidal_sitename) +
   coord_fixed()+
-  theme_bw()+tile_theme 
+  theme_bw()+tile_theme  + theme(legend.position = "top")+
+  theme(plot.margin = margin(0, 0, 0, 0, "cm")) #reduce right margin
 p
 
 
+# Create arrow scheme
+arrow_data <- data.frame(x = 0.5, y = 0.85)
+
+scheme1 <- ggplot(arrow_data, aes(x = x, y = y)) +
+  geom_segment(aes(xend = x, y = 0.85, yend = y - 0.15),
+               arrow = arrow(type = "closed", length = unit(0.1, "inches")),
+               size = 1) +
+  labs(x = NULL, y = NULL) +  # Remove axis labels
+  theme_void() +  # Remove axis lines and ticks
+  annotate("text", x = 0.5, y = 0.92, label = "High \nintertidal", hjust = 0.5, vjust=5.5, size = 2) +
+  annotate("text", x = 0.5, y = 0.68, label = "Low \nintertidal", hjust = 0.5, vjust=-0.2, size = 2)+
+  theme(plot.margin = margin(0, 1, 0, 0, "cm")) #reduce left margin
+
+
+# Merge
+layout_matrix <- matrix(c(1,2), ncol=2)
+g1_full <- gridExtra::grid.arrange(p, scheme1,
+                                   layout_matrix=layout_matrix,
+                                  widths=c(0.9, 0.1))
+g1_full
+
+
+
+ggsave(g1_full, filename = file.path(figdir, "FigX_mussel_expansion.png"), 
+       width = 7, height = 4, units = "in", dpi = 600)
 
 
